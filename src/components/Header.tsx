3cecccd9201976cd2099f19445e319910a708ee2
Ura0x <fabioura.sec@gmail.com>
2025-07-22 00:52:02 -0300

    fix: github repository anchor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
             </div>
             <div>
                 <div>
-                    <a href="https://github.com/Ura0x/WebSimuladorDeMaquinaDeTuring" target="_blank" rel="noopener noreferrer"><img src={github} alt="" className='w-8 hover:cursor-pointer' /></a>
+                    <a href="https://github.com/Ura0x/WebTMS" target="_blank" rel="noopener noreferrer"><img src={github} alt="" className='w-8 hover:cursor-pointer' /></a>
                     
                 </div>
             </div>
